654f818173a8d8bae9275ca88e157f218646ba32
ngayve <hienlegiuse8@yahoo.com>
2025-01-14 22:36:01 -0800

    try fix auth

--- auth.ts
+++ auth.ts
@@ -18,6 +18,19 @@ async function getUser(email: string): Promise<User | undefined> {
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
   providers: [
     Credentials({
       async authorize(credentials) {
